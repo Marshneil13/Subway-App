9826d112d6107708419149f2f540c21362c13794
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-12-22 15:53:08 +0530

    Navbar Created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>SUBWICH</h1>
+      <Navbar/>
     </div>
   );
 }
