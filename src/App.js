bcd67fa71c73c713a1384bff9474f1fda752de73
marshnil_13 <btech10225.20@bitmesra.ac.in>
2023-01-07 12:53:42 +0530

    cart page created with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Link, Switch, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<HomePage />} />
+          <Route path="/cart" exact element={<CartPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ function Navbar() {
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="/cart">
                 <button className="cart-btn">
                   <FaShoppingCart className="cart-icon" />
                   {` ${cartState.cartItems.length}`}
