ad59e025f387ad43e1b2c06a3d2c8a30360ed79e
marshnil_13 <btech10225.20@bitmesra.ac.in>
2023-01-07 23:24:13 +0530

    Cart-page UI part 1

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { cartReducer } from "../reducers/cartReducers";
 
 function CartPage() {
+  const cartState = useSelector((state) => state.cartReducer);
+  const cartItems = cartState.cartItems;
+  //   console.log("CARTiTEMS", cartItems);
   return (
-    <div>
-      <h1>Cart Screen</h1>
+    <div className="cartDiv">
+      <div className="cartDivInner">
+        <h1 className="cartHeading">My Cart</h1>
+        {cartItems.map((item) => {
+          return (
+            <div className="flex-container">
+              <div>
+                <h1>{item.name}</h1>
+                <h1>
+                  Price: {item.quantity} * {item.prices[0][item.varient]} ={" "}
+                  {item.price}
+                </h1>
+              </div>
+              <div></div>
+              <div></div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
