8f6b107101fc7efd87ede1e0afa84d538dfb591c
marshnil_13 <btech10225.20@bitmesra.ac.in>
2023-01-08 14:37:23 +0530

    Cart-page UI part 2

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { cartReducer } from "../reducers/cartReducers";
+import { BiPlus, BiMinus } from "react-icons/bi";
+import { FaTrashAlt } from "react-icons/fa";
 
 function CartPage() {
   const cartState = useSelector((state) => state.cartReducer);
@@ -13,15 +15,29 @@ function CartPage() {
         {cartItems.map((item) => {
           return (
             <div className="flex-container">
-              <div>
-                <h1>{item.name}</h1>
+              <div className="flexDiv m-1 w-100">
+                <h1>
+                  {item.name} [{item.varient}]
+                </h1>
                 <h1>
                   Price: {item.quantity} * {item.prices[0][item.varient]} ={" "}
                   {item.price}
                 </h1>
+                <h1>
+                  Quantity: <BiPlus className="plusIcon" />{" "}
+                  <b>{item.quantity}</b> <BiMinus className="minusIcon" />
+                </h1>
               </div>
-              <div></div>
-              <div></div>
+              <div className="m-10 w-100">
+                <img
+                  src={item.image}
+                  alt="subway"
+                  style={{ height: "100px", width: "100px" }}
+                />
+              </div>
+              <div className="m-1 w-100">
+                <FaTrashAlt className="trashIcon" />
+              </div>
             </div>
           );
         })}
