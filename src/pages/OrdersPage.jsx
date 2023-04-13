4d2153d14c4f42f4d1853864712ab16a2f3ea37d
marshnil_13 <btech10225.20@bitmesra.ac.in>
2023-04-13 21:45:07 +0530

    Orders page designed

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getUserOrders } from "../actions/orderActions";
-
+import Loader from "../components/Loader";
+import Error from "../components/Error";
 const OrdersPage = () => {
   const dispatch = useDispatch();
+  const orderState = useSelector((state) => state.getUserOrdersReducers);
+  const { orders, error, loading } = orderState;
 
   useEffect(() => {
     dispatch(getUserOrders());
@@ -11,6 +14,56 @@ const OrdersPage = () => {
   return (
     <div>
       <h2 style={{ fontSize: "35px" }}>My Orders</h2>
+      <div className="row justify-content-center">
+        {loading && <Loader />}
+        {error && <Error />}
+        {orders &&
+          orders.map((order) => {
+            return (
+              <div
+                className="col-md-8"
+                style={{ backgroundColor: "lightgrey", color: "orangered" }}
+              >
+                <div className="flex-container">
+                  <div className="text-left w-100 m-1">
+                    <h2 style={{ fontSize: "25px", color: "black" }}>Items</h2>
+                    <hr />
+                    {order.orderItems.map((item) => {
+                      return (
+                        <div>
+                          <p>
+                            {item.name} [{item.varient}] * {item.quantity} ={" "}
+                            {item.price}
+                          </p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <div className="text-left w-100 m-1">
+                    <h2 style={{ fontSize: "25px", color: "black" }}>
+                      Address
+                    </h2>
+                    <hr />
+                    <p>Street : {order.shippingAddress.street}</p>
+                    <p>City : {order.shippingAddress.city}</p>
+                    <p>Country : {order.shippingAddress.country}</p>
+                    <p>Pincode : {order.shippingAddress.pincode}</p>
+                  </div>
+                  <div className="text-left w-100 m-1">
+                    <h2 style={{ fontSize: "25px", color: "black" }}>
+                      Order Info
+                    </h2>
+                    <hr />
+                    <p>Order Amount : {order.orderAmount}</p>
+                    <p>Date : {order.createdAt.substring(0, 10)}</p>
+                    <p>Transaction Id : {order.transactionId}</p>
+                    <p>Order Id : {order._id}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
