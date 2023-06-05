8f88898d68d437ce5425f39342d77825cca360ab
Marshneil13 <reachmarshneil@hotmail.com>
2023-06-05 14:44:08 +0530

    github link modification

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,12 @@ const Footer = () => {
     <footer className="myFooter">
       <div className={`text-center p-4`} style={{ backgroundColor: "#ffffff" }}>
         Made with <FaReact style={{ width: "27px", height: "27px" }} /> {`by `}
-        <a className="githubLink" href="https://github.com/Marshneil13">
+        <a
+          className="githubLink"
+          href="https://github.com/Marshneil13"
+          target="_blank"
+          rel="noreferrer"
+        >
           Marshneil
         </a>
       </div>
